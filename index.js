078a59cc4c89d1b05f3836151905683864355b80
bu7ch <dutchr@live.fr>
2017-07-05 00:15:12 +0200

    implanting a simple & static ressource

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ const app  = express();
 
 app.get("/", (req, res) => res.json({status: "NTask API"}));
 
+app.get("/tasks", (req, res) => {
+    res.json({
+      tasks: [
+        {title : "Buy somes shoes"},
+        {title : "Fix notebook"}
+      ]
+    });
+  });
 app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
-// TODO: Implementing a Simple and Static Resource
