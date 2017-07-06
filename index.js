f212f09011d400ef3315a80b4807b3364156f2a2
bu7ch <dutchr@live.fr>
2017-07-06 15:41:27 +0200

    finish loading module

--- index.js
+++ index.js
@@ -4,11 +4,9 @@ import consign from 'consign';
 const PORT = 3000;
 const app  = express();
 
-app.set("json spaces", 4);
-
 consign()
-.include("models");
-  .include("routes");
-  .into("app");
-
-app.listen(PORT, () => console.log(`NTask API - Port ${PORT}`));
+.include("models")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,3 +1,4 @@
+
 module.exports = app => {
   const Tasks = app.models.tasks;
   app.get("/tasks", (req, res) => {
